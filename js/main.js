045f648e52d99260646dbafcb925456d6f9af06a
RiadHossain <riadhossain7464@gmail.com>
2020-06-08 18:34:07 +0600

    new commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let eleID = (id) => {
+    return document.getElementById(id);
+}
+let eleCls = (cls) => {
+    return document.getElementsByClassName(cls);
+}
+let set_style = (elem, styles) => {
+    Object.assign(elem.style, styles);
+}
+const header = eleID('header');
+const intro = eleCls('intro')
+
+let navAnim = () => {
+    let headbound = header.getBoundingClientRect()
+    let introbound = intro[0].getBoundingClientRect()
+    console.log(headbound.top)
+    if (introbound.top < -220)
+        set_style(header, { backgroundColor: 'black', boxShadow: '0 0 0 black' , height:'14vh'})
+    else
+        set_style(header, { backgroundColor: 'transparent', boxShadow: '0 6px 20px black', height:'20vh' })
+
+}
+window.addEventListener('scroll', () => {
+    navAnim()
+
+}) 
